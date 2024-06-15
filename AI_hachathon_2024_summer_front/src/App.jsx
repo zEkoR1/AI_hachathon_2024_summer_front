3e1b8bd6e70a51e8c2f1d3bc29c74fb90463c8fc
auperman-lab <naauperman@gmail.com>
2024-06-16 01:18:43 +0300

    created select product

--- AI_hachathon_2024_summer_front/src/App.jsx
+++ AI_hachathon_2024_summer_front/src/App.jsx
@@ -1,19 +1,30 @@
-import { useState } from 'react'
+// import { useState } from 'react'
 import './App.css'
+import SelectProduct from "./components/selectProduct.jsx";
+import {ChakraProvider, Grid, GridItem} from '@chakra-ui/react';
+
 
 function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>
-        <p>
-          Let the work begin
-        </p>
-      </div>
+    return (
+        <ChakraProvider>
+            <div className="App">
+                <Grid templateColumns='repeat(5, 1fr)' gap={6}>
+                    <GridItem w='100%'  />
+                    <GridItem w='100%' >
+                        <SelectProduct/>
+                    </GridItem>
+                    <GridItem w='100%'  />
+                    <GridItem w='100%'  >
+                        <SelectProduct/>
+                    </GridItem>
+                <GridItem w='100%'  />
+                </Grid>
 
-    </>
-  )
+
+            </div>
+        </ChakraProvider>
+    );
 }
 
-export default App
+export default App;
