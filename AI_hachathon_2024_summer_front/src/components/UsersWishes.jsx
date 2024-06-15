6eb0c4b7f1da82773ec14b97b46e87344164c7f9
zEkoR1 <komarovitstudies@gmail.com>
2024-06-16 01:44:08 +0300

    User Wishes Finished

--- AI_hachathon_2024_summer_front/src/components/UsersWishes.jsx
+++ AI_hachathon_2024_summer_front/src/components/UsersWishes.jsx
@@ -1,22 +1,33 @@
-import React, { useState } from "react";
-import { Input, Text } from "@chakra-ui/react";
+import React, { useState } from 'react';
+import { Box, Textarea, Button, VStack, Text , Spacer} from '@chakra-ui/react';
 
 const UserWishes = () => {
-  const [input, setInput] = useState("");
+  const [input, setInput] = useState('');
 
   const handleChange = (e) => {
     setInput(e.target.value);
   };
 
   return (
-    <>
-      <Input
+    <VStack spacing={4} padding={4}>
+      <Text>
+        What is important for you? Tell us about your use case.
+      </Text>
+      <Textarea
         value={input}
         onChange={handleChange}
-        placeholder='Enter your wishes here...'
-        size='large'
+        placeholder="Enter your wishes here..."
+        size="md"
+        borderColor="gray.300"
+        focusBorderColor="teal.400"
+        height="100px"
+        width="300px"
       />
-    </>
+      <Spacer /> 
+      <Button colorScheme="teal" size="md">
+        Submit
+      </Button>
+    </VStack>
   );
 };
 
