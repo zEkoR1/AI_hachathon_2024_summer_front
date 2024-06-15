253e5372e45e7dcef7e8b3a1ab1507a15aa25651
auperman-lab <naauperman@gmail.com>
2024-06-16 01:19:12 +0300

    created select product component

---
+++ AI_hachathon_2024_summer_front/src/components/selectProduct.jsx
@@ -0,0 +1,37 @@
+import {Input, VStack, Text, Box,} from '@chakra-ui/react'
+
+const SelectProduct = () => {
+
+    // Your component logic here
+    return (
+        <div>
+            <Box
+                bg='purple'
+                borderWidth="2px"
+                borderRadius="xl"
+                boxShadow="md"
+                p={6}
+                borderColor='black'>
+
+                <VStack spacing={4} centerContent>
+                    <Text
+                        fontSize='5xl'
+                        color ='gray.900'>
+                        Product
+                    </Text>
+                    <Input
+                        focusBorderColor='white'
+                        placeholder='Type in the Model'
+                        _placeholder={{ opacity: 1, color: 'black' }}
+                        variant='outline'
+                        borderColor='black'
+
+                    />
+                </VStack>
+            </Box>
+
+        </div>
+    );
+};
+
+export default SelectProduct;
